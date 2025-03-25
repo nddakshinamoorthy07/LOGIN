81a9dcb364222a52e6ba04e8db52bb224c84a7a2
nddakshinamoorthy07 <nddakshinamoorthy07@gmail.com>
2025-03-25 08:30:59 +0530

    ht

--- signapp/views.py
+++ signapp/views.py
@@ -3,9 +3,23 @@ from signapp.models import Signup
 from django.http import HttpResponse
 # Create your views here.
 def signup(request):
-    
+    get_name = request.POST.get('username')
+    get_email = request.POST.get('email')
+    get_password = request.POST.get('password')
+    get_confirm_password = request.POST.get('confirm_password')
+    if get_password == get_confirm_password:
+        Signup.objects.create(name=get_name,email=get_email,password=get_password)
+        return HttpResponse('Signup Success')
+    else:
+        return HttpResponse('Password not matched')
 def login(request):
-     
+    get_email = request.POST.get('email')
+    get_password = request.POST.get('password')
+    try:
+        Signup.objects.get(email=get_email,password=get_password)
+        return HttpResponse('Login Success')
+    except:
+        return HttpResponse('Login Failed')
     
 def home(request):
     return render(request, 'signup/signup.html')
